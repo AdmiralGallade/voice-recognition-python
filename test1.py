0bb99b64c994658f5e24fa946cdf32f78d9828c6
AdmiralGallade <mailforkushagra2@gmail.com>
2020-07-10 16:02:46 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -9,3 +9,19 @@ with f as source:
     audio= r.record(source)
 
 r.recognize_google(audio)
+
+
+
+with f as source:
+    audio1 = r.record(source, duration=4)
+    audio2= r.record(source,duration=2)
+
+r.recognize_google(audio1)
+r.recognize_google(audio2)
+
+
+with f as source:
+    r.adjust_for_ambient_noise(source)      #cuts noise
+    audio3 = r.record(source, offset=4, duration=3)
+
+r.recognize_google(audio3) 
